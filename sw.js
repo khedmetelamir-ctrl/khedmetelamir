d02df6a7225fd63ea0cd54e6880ad6c91ee5e486
khedmetelamir-ctrl <khedmetelamir@gmail.com>
2026-09-26 05:09:56 +0300

    Refresh PWA for note RPC fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kea-v-notes-save-delete-12';
+const CACHE='kea-v-notes-rpc-13';
 const SHELL=['./','./index.html','./logo.png','./logo-192.png','./logo-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
