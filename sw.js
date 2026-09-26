fe8d2946467e5957aa779750993b8365fddab392
khedmetelamir-ctrl <khedmetelamir@gmail.com>
2026-09-26 14:31:50 +0300

    Refresh PWA after audit fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kea-v-notes-rpc-13';
+const CACHE='kea-v-audit7-14';
 const SHELL=['./','./index.html','./logo.png','./logo-192.png','./logo-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
